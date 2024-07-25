b5b8fb03145a9ba2b2d2542bc5b7cb4bd03de0e8
612teres <612teres@gmail.com>
2024-07-25 19:19:50 +0300

    Making progress

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/fabian-teres/Data_Visualizer/venv"
+set -gx VIRTUAL_ENV "/home/fabian-teres/Data_Visualizer/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
